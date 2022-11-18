2cc106fb834c1001d6b9f0b6ff656f3116453ef3
Djal4 <aleksandar.djalovic@quantoxtechnology.com>
2022-11-18 23:49:12 +0100

    Sanctum.

--- back/database/migrations/2014_10_12_000000_create_users_table.php
+++ back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->date('year_of_birth');
-            $table->string('photo');//photo path
+            $table->string('photo')->nullable();//photo path
         });
     }
 
